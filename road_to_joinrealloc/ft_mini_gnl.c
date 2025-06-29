baf06c593f6b62adf434e8185f9c61a09cdb411a
Kevin Assassin <kassassi@f0r4s19.clusters.42paris.fr>
2025-06-29 17:51:17 +0200

    oskour

--- road_to_joinrealloc/ft_mini_gnl.c
+++ road_to_joinrealloc/ft_mini_gnl.c
@@ -6,7 +6,7 @@
 /*   By: kassassi <kassassi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 16:51:54 by kassassi          #+#    #+#             */
-/*   Updated: 2025/06/29 16:22:52 by kassassi         ###   ########.fr       */
+/*   Updated: 2025/06/29 17:50:33 by kassassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,13 @@
 
 char	*ft_mini_gnl(int fd)
 {
-}
+	static char *stash;
+	char		*cleanline;
+	char		buffer[7];
+	ssize_t		nline_pos;
+	ssize_t		bytes_loot;
+	size_t		i;
+	
+	if (stash)
+
+
